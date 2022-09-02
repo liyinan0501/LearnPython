5725c9b7b681d7d6450bb48d5338046c653ab3e2
Yinan Li <gabriel.li5566@gmail.com>
2022-09-02 22:47:00 +0300

    update Assignment

---
+++ 07-Assignment.py
@@ -0,0 +1,20 @@
+"""
+TODO Assignment
+"""
+# Multiple Variables Assignment
+num1, float1, str1 = 10, 0.33, "Hello World"
+print(num1)  # 10
+print(float1)  # 0.33
+print(str1)  # Hello World
+
+# Multiple Variables Assignment with a Same Value
+a = b = 10
+print(a)  # 10
+print(b)  # 10
+
+# Addition Assignment
+# +=, -+, *=, /=, //=, %=, **=,
+c = 11
+c *= 1 + 2
+# 11 *= 3
+print(c)  # 33
