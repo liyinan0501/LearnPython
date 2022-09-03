0e374c04f22eb93b9f1842a17f4a66dd97e4e7b7
Yinan Li <gabriel.li5566@gmail.com>
2022-09-04 00:03:02 +0300

    update Ternary Operators

---
+++ 12-Ternary Operators.py
@@ -0,0 +1,14 @@
+"""
+TODO Ternary Operators
+"""
+a = 1
+b = 2
+
+c = a if a > b else b
+print(c)  # 2
+
+numA = 10
+numB = 6
+
+result = numA - numB if numA > numB else numB - numA
+print(result)  # 4
