ee65f6aa9db7585a24de25eaff88b1ba12846d50
Yinan Li <gabriel.li5566@gmail.com>
2022-09-03 19:36:33 +0300

    update Condition

--- 09-Condition.py
+++ 09-Condition.py
@@ -1,6 +1,11 @@
-from tkinter.messagebox import YES
-
-
-age = 20
-if age >= 18:
-    print("Yes")
+"""
+TODO Condition
+"""
+age = int(input("Your age: "))
+if age < 18:
+    print("You are under age.")
+elif 18 <= age <= 60:
+    print("You are adult.")
+else:
+    print("You are retired.")
+print("close....")
